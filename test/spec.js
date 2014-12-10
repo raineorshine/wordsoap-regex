debe3dae0d6576087b1551b4e6310b14df27f2cb
metaraine <raineorshine@gmail.com>
2014-12-09 22:19:56 -0700

    Remove sample and msoClass.

--- test/spec.js
+++ test/spec.js
@@ -1,12 +1,3 @@
 'use strict'
 var assert = require('insist')
 var wordsoap = require('../index.js')
-
-describe('msoClasses', function() {
-	it('should remove Mso- classes', function () {
-		var input = '<p class=MsoNormal>test</p>'
-		var output = '<p>test</p>'
-		assert.notEqual(wordsoap.msoClass, undefined, 'msoClass is not defined')
-		assert.equal(input.replace(wordsoap.msoClass, ''), output)
-	})
-})
