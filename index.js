e42a5ef11209b31706d5642717d30f8f034ca37a
metaraine <raineorshine@gmail.com>
2014-12-09 17:43:49 -0700

    Add msoTags and msoAttributes

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict'
 
 module.exports = {
+	// from http://tim.mackey.ie/CleanWordHTMLUsingRegularExpressions.aspx
+	msoTags: /<[\/]?(font|span|xml|del|ins|[ovwxp]:\w+)[^>]*?>/,
+	msoAttributes: /<([^>]*)(?:class|lang|style|size|face|[ovwxp]:\w+)=(?:'[^']*'|""[^""]*""|[^\s>]+)([^>]*)>/,
+
 	msoClass: /\W+class=Mso\w*\b/
 }
--- test/spec.js
+++ test/spec.js
@@ -2,9 +2,11 @@
 var assert = require('insist')
 var wordsoap = require('../index.js')
 
-it('should remove Mso- classes', function () {
-	var input = '<p class=MsoNormal>test</p>'
-	var output = '<p>test</p>'
-	assert.notEqual(wordsoap.msoClass, undefined, 'msoClass is not defined')
-	assert.equal(input.replace(wordsoap.msoClass, ''), output)
+describe('msoClasses', function() {
+	it('should remove Mso- classes', function () {
+		var input = '<p class=MsoNormal>test</p>'
+		var output = '<p>test</p>'
+		assert.notEqual(wordsoap.msoClass, undefined, 'msoClass is not defined')
+		assert.equal(input.replace(wordsoap.msoClass, ''), output)
+	})
 })
